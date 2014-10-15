95488e6f1233afae624ae92bc95025f86241ee1c
sasaki <sasaki@backyard.com>
2014-10-15 15:18:11 +0900

    add time log and change org format

--- gcom.sh
+++ gcom.sh
@@ -1,13 +1,36 @@
 #!/bin/sh
 
-echo start 100 time git commit
+nlog=gcom.log
+head="user,system,elapsed,cpu,text,data,max,inputs,outputs,major,minor,swaps"
+fmat="%U, %S, %E, %P, %X, %D, %M, %I, %O, %F, %R, %W"
+echo $nlog
 
-for i in `seq 1 100`
+echo start 10000 time git commit >$nlog
+echo "$head">>$nlog
+echo "$fmat" >>$nlog
+
+for i in `seq 1 10000`
 do
+  echo $i
   echo i > $i.txt
   date  >> $i.txt
+#  (time git add $i.txt) >>$nlog 2>&1
   git add $i.txt
-  git commit -m "add commit $i.txt"
+#  (time git commit -m "add commit $i.txt") >>$nlog 2>&1
+  (time -f "$fmat" git commit -m "add commit $i.txt") 2>>$nlog
+  rm $i.txt
 done
 
-echo end   100 time git commit
+echo end   10000 time git commit >>$nlog
+
+exit
+
+# %U user %S system %E elapsed %P CPU (%X text+%D data %M max)k
+# %I inputs+%O outputs (%F major+%R minor)pagefaults %W swaps
+#
+# echo "user,system,elapsed,cpu,text,data,max,inputs,outputs,major,minor,swaps"
+# echo "%U, %S, %E, %P, %X, %D, %M, %I, %O, %F, %R, %W"
+# -f "%U, %S, %E, %P, %X, %D, %M, %I, %O, %F, %R, %W"
+ 
+
+
